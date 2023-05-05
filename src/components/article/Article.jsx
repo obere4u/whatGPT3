1d35bd3cec079002d3e20fc26f2123b34624dd99
Nwosa <obere4u@gmail.com>
2023-05-05 10:43:15 +0100

    Created App.css and index.css and gave them custom styles

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './artcle.css'
+
+function Article() {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
---
+++ src/containers/possibilities/Possibilities.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './posibilities.css'
+
+function Possibities() {
+  return (
+    <div>Possibities</div>
+  )
+}
+
+export default Possibities
